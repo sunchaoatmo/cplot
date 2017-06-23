d246b452f59aeb143d8afa3792cd9fb47051c01e
chao <sunchao@umd.edu>
2017-06-23 12:00:06 -0400

    enable the taylor plot output is avaible to both style

--- seasonal.py
+++ seasonal.py
@@ -2,14 +2,14 @@ from context import reginalmetfield
 class seasonal_data(reginalmetfield):
   def __init__(self,period,vnames,cases,nlevel,cutpoints,neof,
                method,plottype,shapefile,datapath,obsname,GCM_name,Time_control,
-               wrfinputfile,landmaskfile,masktype,PLOT):
+               wrfinputfile,landmaskfile,masktype,PLOT,Taylor,regmapfile):
     reginalmetfield.__init__(self,period,vnames,cases,nlevel,cutpoints,neof,
                method,plottype,shapefile,datapath,obsname,GCM_name,Time_control,
-               wrfinputfile,landmaskfile,masktype,PLOT)
+               wrfinputfile,landmaskfile,masktype,PLOT,Taylor,regmapfile=regmapfile)
 
 
   def Output(self):
-    if self.plottype=="CTaylor": 
+    if self.plottype=="Taylor": 
       from cstaylor import writedata
       for vname in self.vnames:
         writedata(self,vname)
