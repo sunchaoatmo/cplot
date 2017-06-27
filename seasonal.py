e9a160198472612f2b5e9100f958ac753114998f
chao <sunchao@umd.edu>
2017-06-27 18:36:12 -0400

    put the settings in one dict for seasonal class

--- seasonal.py
+++ seasonal.py
@@ -1,11 +1,15 @@
 from context import reginalmetfield
 class seasonal_data(reginalmetfield):
+  def __init__(self,settings):
+    reginalmetfield.__init__(self,settings)
+  """
   def __init__(self,period,vnames,cases,nlevel,cutpoints,neof,
                method,plottype,shapefile,datapath,obsname,GCM_name,Time_control,
                wrfinputfile,landmaskfile,masktype,PLOT,Taylor,regmapfile):
     reginalmetfield.__init__(self,period,vnames,cases,nlevel,cutpoints,neof,
                method,plottype,shapefile,datapath,obsname,GCM_name,Time_control,
                wrfinputfile,landmaskfile,masktype,PLOT,Taylor,regmapfile=regmapfile)
+  """
 
 
   def Output(self):
